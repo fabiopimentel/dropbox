c5ae0205496ef8e9dc4d3e69162b14a308b4979d
Fábio Pimentel <fabiolp4@gmail.com>
2011-10-06 17:58:19 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dropbox::Application.config.secret_token = '1be90a50a06b9fa9665e7ae8b86cc6b780b2e5a56300318d7c9b85f75a79203e7e0bc889d6dbd02b5405a7967b071f4231c7eff66cfb766e6612b16280f14fa6'
